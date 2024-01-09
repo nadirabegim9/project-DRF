f78204b644c89ce98b6483b16e29111ce1f978bc
надирабегим кожоярова <kokozoarovanadirabegim@gmail.com>
2024-01-09 18:20:01 +0000

    djangorestframework

--- django_project/women/admin.py
+++ django_project/women/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Women, Category
+
+admin.site.register(Women)
+admin.site.register(Category)
--- django_project/women/views.py
+++ django_project/women/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Women
+from .serializers import WomenSerializer
+
+
+class WomenAPIView(APIView):
+    def get(self, request):
+        lst = Women.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        return Response({'title': 'Гульнур Сатылганова'})
+
+
+# class WomenAPIView(generics.ListAPIView):
+#     queryset = Women.objects.all()
+#     serializer_class = WomenSerializer
+
